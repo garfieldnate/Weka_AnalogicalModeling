115dc1d7594d1127466a10dd4d2c860c3529b41e
garfieldnate <garfieldnate@7fbc6b3d-0102-4d0c-9cd4-7308afd6a1dd>
2012-01-18 03:14:57 +0000

    Unit test for SubsetIterator.
    git-svn-id: svn://svn.code.sf.net/p/javaam/code/trunk@8 7fbc6b3d-0102-4d0c-9cd4-7308afd6a1dd

---
+++ Analogical Modeling/src/edu/byu/am/test/TestSubsetIterator.java
@@ -0,0 +1,33 @@
+package edu.byu.am.test;
+
+import static org.junit.Assert.*;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+import org.junit.Test;
+
+import edu.byu.am.lattice.SubsetIterator;
+
+public class TestSubsetIterator {
+
+	@Test
+	public void testIterator() {
+		Set<Integer> cmp = new TreeSet<Integer>();
+		//iterating over 1001011
+		cmp.add(107);//1101011
+		cmp.add(123);//1111011
+		cmp.add(91);//1011011
+		cmp.add(95);//1011111
+		cmp.add(111);//1111111
+		cmp.add(127);//1101111
+		cmp.add(79);//1001111
+		SubsetIterator si = new SubsetIterator(75, 7);
+		Set<Integer> ints = new TreeSet<Integer>();
+		while(si.hasNext())
+			ints.add(si.next());
+		assertEquals(cmp,ints);
+		
+	}
+
+}
