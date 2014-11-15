b853844c3443c31a99b2f409b0e7aeda514c3858
Nathan Glenn <garfieldnate@gmail.com>
2014-11-15 23:01:39 +0900

    chapter3 test
    Test that the chapter3 (green book) data is correctly classified.

--- Analogical Modeling/src/test/weka/classifiers/lazy/AnalogicalModelingTest.java
+++ Analogical Modeling/src/test/weka/classifiers/lazy/AnalogicalModelingTest.java
@@ -18,33 +18,51 @@ package weka.classifiers.lazy;
 
 import weka.classifiers.AbstractClassifierTest;
 import weka.classifiers.Classifier;
+import weka.core.Instances;
+import weka.core.converters.ConverterUtils.DataSource;
 
-import junit.framework.Test;
+import org.junit.Test;
+
 import junit.framework.TestSuite;
 
 /**
- * Tests AnalogicalModeling. Run from the command line with:<p>
- * java weka.classifiers.lazy.AnalogicalModelingTest
- *
+ * Tests AnalogicalModeling.
+ * 
  * @author <a href="mailto:garfieldnate@gmail.com">Nate Glenn</a>
- * @version $Revision: 8034 $
  */
 public class AnalogicalModelingTest extends AbstractClassifierTest {
 
-  public AnalogicalModelingTest(String name) { super(name);  }
+	public AnalogicalModelingTest(String name) {
+		super(name);
+	}
 
-  /** Creates a default AnalogicalModeling */
-  @Override
-public Classifier getClassifier() {
-    return new AnalogicalModeling();
-  }
+	/** Creates a default AnalogicalModeling */
+	@Override
+	public Classifier getClassifier() {
+		return new AnalogicalModeling();
+	}
+	
+	private final double DELTA = 1e-10;
+	@Test
+	public void testChapter3data() throws Exception {
+		DataSource source = new DataSource("data/ch3example.arff");
+		Instances train = source.getDataSet();
+		train.setClassIndex(train.numAttributes() - 1);
+		
+		source = new DataSource("data/ch3exampleTest.arff");
+		Instances test = source.getDataSet();
+		test.setClassIndex(test.numAttributes() - 1);
+		
+		Classifier am = getClassifier();
+		am.buildClassifier(train);
+		double[] prediction = am.distributionForInstance(test.firstInstance());
+		assertEquals("distribution given for two classes", prediction.length, 2);
+		assertEquals(prediction[0], 0.6923076923076923, DELTA);
+		assertEquals(prediction[1], 0.3076923076923077, DELTA);
+	}
 
-  public static Test suite() {
-    return new TestSuite(AnalogicalModelingTest.class);
-  }
-
-  public static void main(String[] args){
-    junit.textui.TestRunner.run(suite());
-  }
+	public static junit.framework.Test suite() {
+		return new TestSuite(AnalogicalModelingTest.class);
+	}
 
 }
