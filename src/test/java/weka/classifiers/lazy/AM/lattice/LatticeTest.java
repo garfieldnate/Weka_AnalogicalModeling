3ea7772e0f27c32c0cab566648aa3a29ff6def76
Nathan Glenn <garfieldnate@gmail.com>
2014-12-12 15:58:36 +0900

    make LatticeTest more flexible and extensible
    Refactor and use reflection so that one test can involve multiple
    datasets and classifications. Instead of classifying a data set with
    different lattices and returning these as parameters, provide
    constructor objects and their required arguments.

--- src/test/java/weka/classifiers/lazy/AM/lattice/LatticeTest.java
+++ src/test/java/weka/classifiers/lazy/AM/lattice/LatticeTest.java
@@ -2,6 +2,7 @@ package weka.classifiers.lazy.AM.lattice;
 
 import static org.junit.Assert.assertEquals;
 
+import java.lang.reflect.Constructor;
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -11,6 +12,7 @@ import java.util.List;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
 
 import weka.classifiers.lazy.AM.AMUtils;
 import weka.classifiers.lazy.AM.TestUtils;
@@ -21,44 +23,63 @@ import weka.core.Instances;
 @RunWith(Parameterized.class)
 public class LatticeTest {
 
-	private ILattice testLattice;
-	private static Instances train;
-	private static Instance test;
-
-	public LatticeTest(ILattice lattice, String implName) {
-		testLattice = lattice;
-	}
+	@Parameter(0)
+	public String testName;
+	@Parameter(1)
+	public Constructor<ILattice> constructor;
+	@Parameter(2)
+	public Object[] ctorArgs;
 
 	/**
 	 * 
-	 * @return Implementations of Lattice, and their class names
+	 * @return A collections of argument lists for running tests
+	 *         <ol>
+	 *         <li>arg[0] is the test name;</li>
+	 *         <li>arg[1] is an {@link ILattice} {@link Constructor} object</li>
+	 *         <li>arg[2] is another Object[] containing the arguments to be
+	 *         passed to the constructor; element 0 is left null so that it can
+	 *         be set to the desired {@link SubcontextList}.</li>
+	 *         </ol>
 	 * @throws Exception
 	 */
-	@Parameterized.Parameters(name = "{1}")
+	@Parameterized.Parameters(name = "{0}")
 	public static Collection<Object[]> instancesToTest() throws Exception {
-		train = TestUtils.getDataSet(TestUtils.CHAPTER_3_TRAIN);
-		test = TestUtils.getInstanceFromFile(TestUtils.CHAPTER_3_TEST, 0);
+		Collection<Object[]> parameters = new ArrayList<>();
 
-		Labeler labeler = new Labeler(MissingDataCompare.MATCH, test, false);
+		String testName;
+		Constructor<? extends ILattice> latticeConstructor;
+		Object[] constructorArgs;
 
-		SubcontextList subList = new SubcontextList(labeler, train);
+		testName = BasicLattice.class.getSimpleName();
+		latticeConstructor = BasicLattice.class
+				.getDeclaredConstructor(SubcontextList.class);
+		constructorArgs = new Object[1];
+		parameters.add(new Object[] { testName, latticeConstructor,
+				constructorArgs });
 
-		Collection<Object[]> parameters = new ArrayList<>(3);
-		parameters.add(new Object[] { new BasicLattice(subList),
-				BasicLattice.class.getSimpleName() });
-		parameters.add(new Object[] { new DistributedLattice(subList),
-				DistributedLattice.class.getSimpleName() });
-		parameters
-				.add(new Object[] {
-						new DistributedLattice(subList, 2),
-						DistributedLattice.class.getSimpleName()
-								+ ": 2 sub-lattices" });
-		// 10 should be reduced to 3, since there are only three attributes.
-		parameters
-				.add(new Object[] {
-						new DistributedLattice(subList, 10),
-						DistributedLattice.class.getSimpleName()
-								+ ": 10 sub-lattices" });
+		// default number of lattices
+		testName = DistributedLattice.class.getSimpleName();
+		latticeConstructor = DistributedLattice.class
+				.getDeclaredConstructor(SubcontextList.class);
+		parameters.add(new Object[] { testName, latticeConstructor,
+				new Object[1] });
+
+		// specify 2 lattices
+		testName = DistributedLattice.class.getSimpleName()
+				+ ": 2 sub-lattices";
+		latticeConstructor = DistributedLattice.class.getDeclaredConstructor(
+				SubcontextList.class, int.class);
+		constructorArgs = new Object[] { null, new Integer(2) };
+		parameters.add(new Object[] { testName, latticeConstructor,
+				constructorArgs });
+
+		// specify 10 lattices, which will have to be reduced in some cases to
+		// match smaller cardinality data sets
+		testName = DistributedLattice.class.getSimpleName()
+				+ ": 10 sub-lattices";
+		constructorArgs = new Object[] { null, new Integer(10) };
+		parameters.add(new Object[] { testName, latticeConstructor,
+				constructorArgs });
 
 		return parameters;
 	}
@@ -66,6 +87,17 @@ public class LatticeTest {
 	@SuppressWarnings("serial")
 	@Test
 	public void testChapter3Data() throws Exception {
+		Instances train = TestUtils.getDataSet(TestUtils.CHAPTER_3_TRAIN);
+		Instance test = TestUtils.getInstanceFromFile(TestUtils.CHAPTER_3_TEST,
+				0);
+
+		Labeler labeler = new Labeler(MissingDataCompare.MATCH, test, false);
+
+		SubcontextList subList = new SubcontextList(labeler, train);
+
+		ctorArgs[0] = subList;
+		ILattice testLattice = constructor.newInstance(ctorArgs);
+
 		List<Supracontext> supras = testLattice.getSupracontextList();
 		assertEquals(3, supras.size());
 
