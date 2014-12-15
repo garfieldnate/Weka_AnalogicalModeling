cc824fba9e0d224b83f841bea19429d5c1ec36a5
Nathan Glenn <garfieldnate@gmail.com>
2014-12-16 02:17:54 +0900

    add masking functionality to Labeler
    Get rid of the idea of a `Mask` object, which was problematice for
    encapsulation. Instead, allow the `Labeler`s to `partition` `Label`
    objects that they know how to deal with. Implent it in the
    `BitSetLabeler`, but don't unify it with `IntLabeler` or test it yet.

--- src/main/java/weka/classifiers/lazy/AM/lattice/BitSetLabeler.java
+++ src/main/java/weka/classifiers/lazy/AM/lattice/BitSetLabeler.java
@@ -10,6 +10,7 @@ import weka.core.Instance;
 public class BitSetLabeler extends Labeler {
 	private final Set<Integer> ignoreSet;
 	private int classIndex;
+	private Partition[] masks;
 
 	/**
 	 * 
@@ -71,5 +72,65 @@ public class BitSetLabeler extends Labeler {
 			index++;
 		}
 		return new BitSetLabel(label, getCardinality());
+	}
+
+	@Override
+	public Label partition(Label label, int partitionIndex) {
+		if (partitionIndex > numPartitions() || partitionIndex < 0)
+			throw new IllegalArgumentException("Illegal partition index: "
+					+ partitionIndex);
+		if (label.getCardinality() != getCardinality())
+			throw new IllegalArgumentException("Label cardinality is "
+					+ label.getCardinality() + " but labeler cardinality is "
+					+ getCardinality());
+		if (!(label instanceof BitSetLabel))
+			throw new IllegalArgumentException("This labeler can only handle "
+					+ BitSetLabel.class.getCanonicalName()
+					+ "s; input label was an instance of "
+					+ label.getClass().getCanonicalName());
+
+		// create and cache the masks if they have not be created yet
+		if (masks == null) {
+			masks = new Partition[numPartitions()];
+			Span[] spans = partitions();
+			for (int i = 0; i < numPartitions(); i++) {
+				masks[i] = new Partition(spans[i].getStart(), spans[i].getEnd()
+						- spans[i].getStart());
+			}
+		}
+
+		// loop through the bits and set the unmatched ones
+		Partition mask = masks[partitionIndex];
+		BitSet newLabel = new BitSet(mask.getCardinality());
+		int index = mask.getStartIndex();
+		for (int i = 0; i < mask.getCardinality(); i++) {
+			if (!label.matches(index))
+				newLabel.set(i);
+			index++;
+		}
+		// TODO: would it be possible/worth while to create an IntLabel if
+		// the cardinality were small enough?
+		return new BitSetLabel(newLabel, mask.getCardinality());
+	}
+
+	/**
+	 * Private class for storing label paritions
+	 */
+	private class Partition {
+		private int startIndex;
+		private int cardinality;
+
+		public Partition(int i, int c) {
+			startIndex = i;
+			cardinality = c;
+		}
+
+		public int getStartIndex() {
+			return startIndex;
+		}
+
+		public int getCardinality() {
+			return cardinality;
+		}
 	}
 }
--- src/main/java/weka/classifiers/lazy/AM/lattice/Labeler.java
+++ src/main/java/weka/classifiers/lazy/AM/lattice/Labeler.java
@@ -2,6 +2,7 @@ package weka.classifiers.lazy.AM.lattice;
 
 import weka.core.Instance;
 
+//TODO: next, unify with IntLabler and IntLabel, and add tests.
 /**
  * This class is used to assign context labels to training instances by
  * comparison with the instance being classified.
@@ -12,6 +13,10 @@ public abstract class Labeler {
 	protected boolean ignoreUnknowns;
 	protected MissingDataCompare mdc = null;
 	protected Instance testInstance = null;
+	/**
+	 * The default number of lattices to use during distributional processing.
+	 */
+	private static final int NUM_LATTICES = 4;
 
 	/**
 	 * @return The cardinality of the generated labels, or how many instance
@@ -58,4 +63,83 @@ public abstract class Labeler {
 	 *             set.
 	 */
 	public abstract Label label(Instance data);
+
+	/**
+	 * In distributed processing, it is necessary to split labels into
+	 * partitions. This method returns a partition for the given label. A full
+	 * label is partitioned into pieces 0 through {@link #numPartitions()}, so
+	 * code to process labels in pieces should look like this: <code>
+	 * 	Label myLabel = myLabeler.label(myInstance);
+	 * 	for(int i = 0; i < myLabeler.numPartitions(); i++)
+	 * 		process(myLabeler.partition(myLabel, i);
+	 * </code>
+	 * 
+	 * @param partitionIndex
+	 *            index of the partition to return
+	 * @return a new label representing a portion of the attributes represented
+	 *         by the input label.
+	 * @throws IllegalArgumentException
+	 *             if the partitionIndex is greater than
+	 *             {@link #numPartitions()} or less than zero.
+	 * @throws IllegalArgumentException
+	 *             if the input label is not compatible with this labeler.
+	 */
+	public abstract Label partition(Label label, int partitionIndex);
+
+	/**
+	 * @return The number of label partitions available via {@link #partition}
+	 */
+	public int numPartitions() {
+		// just for now, we use a maximum of 4 partitions
+		if (getCardinality() < NUM_LATTICES)
+			return getCardinality();
+		else
+			return NUM_LATTICES;
+	}
+
+	protected Span[] partitions() {
+		Span[] spans = new Span[numPartitions()];
+
+		int spanSize = (int) Math.floor((double) getCardinality()
+				/ numPartitions());
+		// an extra bit will be given to remainder masks, since numMasks
+		// probably does not divide cardinality
+		int remainder = getCardinality() % numPartitions();
+		int index = 0;
+		for (int i = 0; i < numPartitions(); i++) {
+			int inc = (i < remainder) ? spanSize + 1 : spanSize;
+			spans[i] = new Span(index, inc);
+			index += inc;
+		}
+		return spans;
+	}
+
+	/**
+	 * Simple class for storing index spans.
+	 * 
+	 */
+	protected class Span {
+		private int start;
+		private int end;
+
+		protected Span(int s, int e) {
+			start = s;
+			end = e;
+		}
+
+		/**
+		 * @return The beginning of the span
+		 */
+		protected int getStart() {
+			return start;
+		}
+
+		/**
+		 * @return The end of the span
+		 */
+		protected int getEnd() {
+			return end;
+		}
+	}
+
 }
